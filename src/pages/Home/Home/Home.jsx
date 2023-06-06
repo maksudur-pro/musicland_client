86b21bf70046fa12601770378faa2a1fa6c8cac2
unknown <ajoy9532@gmail.com>
2023-06-07 01:47:36 +0600

    created basic setup

---
+++ src/pages/Home/Home/Home.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import { Helmet } from "react-helmet-async";
+
+const Home = () => {
+  return (
+    <div>
+      <Helmet>
+        <title>Music Land | Home</title>
+      </Helmet>
+      <h1>this is home</h1>
+    </div>
+  );
+};
+
+export default Home;
---
+++ src/pages/Shared/Header/Header.jsx
@@ -0,0 +1,93 @@
+import React from "react";
+import { Link, NavLink } from "react-router-dom";
+
+const Header = () => {
+  const navOption = (
+    <>
+      <li>
+        <NavLink
+          to="/"
+          className={({ isActive }) =>
+            isActive ? "text-blue-500" : "default"
+          }>
+          Home
+        </NavLink>
+      </li>
+      <li>
+        <NavLink
+          to="/instructors"
+          className={({ isActive }) =>
+            isActive ? "text-blue-500" : "default"
+          }>
+          Instructors
+        </NavLink>
+      </li>
+      <li>
+        <NavLink
+          to="/classes"
+          className={({ isActive }) =>
+            isActive ? "text-blue-500" : "default"
+          }>
+          Classes
+        </NavLink>
+      </li>
+      <li>
+        <NavLink
+          to="/dashboard"
+          className={({ isActive }) =>
+            isActive ? "text-blue-500" : "default"
+          }>
+          Dashboard
+        </NavLink>
+      </li>
+      <li>
+        <NavLink
+          to="/login"
+          className={({ isActive }) =>
+            isActive ? "text-blue-500" : "default"
+          }>
+          Login
+        </NavLink>
+      </li>
+    </>
+  );
+
+  return (
+    <div className="navbar bg-base-100">
+      <div className="navbar-start">
+        <div className="dropdown">
+          <label tabIndex={0} className="btn btn-ghost lg:hidden">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              className="h-5 w-5"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor">
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
+                d="M4 6h16M4 12h8m-8 6h16"
+              />
+            </svg>
+          </label>
+          <ul
+            tabIndex={0}
+            className="menu font-semibold text-lg menu-sm dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52">
+            {navOption}
+          </ul>
+        </div>
+        <p className="text-2xl font-bold ml-[160px] lg:ml-0">
+          Music<span className="text-error">Land</span>
+        </p>
+      </div>
+      <div className="navbar-end hidden lg:flex">
+        <ul className="menu menu-horizontal px-1 text-lg font-semibold">
+          {navOption}
+        </ul>
+      </div>
+    </div>
+  );
+};
+
+export default Header;
