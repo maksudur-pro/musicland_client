739ce92347598ae51e97355bf85279b0bb1a39de
unknown <ajoy9532@gmail.com>
2023-06-10 21:41:31 +0600

    fixed manageClass

--- src/pages/Dashboard/Admin/ManageClasses.jsx
+++ src/pages/Dashboard/Admin/ManageClasses.jsx
@@ -1,122 +1,159 @@
-import React, { useState } from "react";
-import useClass from "../../../Hooks/useClass";
-import { FaCheck, FaComments, FaTimes } from "react-icons/fa";
+import { useState } from "react";
+import { FaCheck, FaTimes, FaComments, FaCircle } from "react-icons/fa";
+import UseClass from "../../../Hooks/UseClass";
 import axios from "axios";
+import { MdPendingActions } from "react-icons/md";
 
 const ManageClasses = () => {
-  const [data, refetch] = useClass();
-  const [feedbackModalOpen, setFeedbackModalOpen] = useState(false);
-  const [selectedClass, setSelectedClass] = useState(null);
-  const [feedback, setFeedback] = useState("");
+  const [data, refetch] = UseClass();
+  console.log(data);
 
-  // approved status
-  const approveClass = async (id) => {
+  const handleApprove = async (classId) => {
     try {
-      await axios.patch(`http://localhost:5000/classes/approve/${id}`);
-      refetch();
+      await axios.patch(`http://localhost:5000/classes/approve/${classId}`);
+      refetch(); // Refetch the updated data
     } catch (error) {
       console.error("Error approving class:", error);
     }
   };
 
-  // denied class
-  const denyClass = async (id) => {
+  const handleDeny = async (classId) => {
     try {
-      await axios.patch(`http://localhost:5000/classes/deny/${id}`);
-      refetch();
+      await axios.patch(`http://localhost:5000/classes/deny/${classId}`);
+      refetch(); // Refetch the updated data
     } catch (error) {
       console.error("Error denying class:", error);
     }
   };
+
+  const handleFeedback = async (classId, feedback) => {
+    if (!feedback) {
+      alert("Feedback cannot be empty");
+      return;
+    }
+    try {
+      await axios.post(`http://localhost:5000/classes/feedback/${classId}`, {
+        feedback,
+      });
+      refetch(); // Refetch the updated data
+    } catch (error) {
+      console.error("Error sending feedback:", error);
+    }
+  };
+
+  const [feedbackModalOpen, setFeedbackModalOpen] = useState(false);
+  const [selectedClass, setSelectedClass] = useState(null);
+  const [feedbackText, setFeedbackText] = useState("");
 
   const openFeedbackModal = (classObj) => {
     setSelectedClass(classObj);
     setFeedbackModalOpen(true);
   };
 
-  const sendFeedback = async () => {
-    if (!selectedClass || !feedback) return;
-
-    try {
-      await axios.post(
-        `http://localhost:5000/classes/feedback/${selectedClass._id}`,
-        {
-          feedback: feedback,
-        }
-      );
-      setFeedbackModalOpen(false);
-      setFeedback("");
-      setSelectedClass(null);
-      refetch();
-    } catch (error) {
-      console.error("Error sending feedback:", error);
-    }
+  const closeFeedbackModal = () => {
+    console.log("Closing feedback modal");
+    setSelectedClass(null);
+    setFeedbackText("");
+    setFeedbackModalOpen(false);
   };
 
   return (
-    <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
-      {data.map((classObj) => (
-        <div key={classObj._id} className="bg-white p-4 shadow-md rounded-lg">
-          <img
-            src={classObj.classImg}
-            alt={classObj.courseName}
-            className="w-full rounded-xl object-cover mb-4"
-          />
-          <h2 className="text-xl font-bold mb-2">{classObj.courseName}</h2>
-          <p className="mb-2">Instructor: {classObj.instructor_name}</p>
-          <p className="mb-2">Email: {classObj.email}</p>
-          <p className="mb-2">Available seats: {classObj.seats}</p>
-          <p className="mb-2">Price: ${classObj.price}</p>
-          <p className="mb-2">Status: {classObj.status}</p>
-          <div className="flex">
-            {classObj.status === "pending" && (
-              <>
+    <>
+      <div>
+        <h1 className="text-3xl font-bold mb-4">Manage Classes</h1>
+        {/* classes cards */}
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
+          {data.map((classObj) => (
+            <div
+              key={classObj._id}
+              className="bg-white p-4 shadow-md rounded-lg">
+              <img
+                src={classObj.classImg}
+                className="w-full object-cover mb-4"
+              />
+              <h2 className="text-xl font-bold mb-2">{classObj.courseName}</h2>
+              <p className="mb-2"> Instructor: {classObj.instructor_name}</p>
+              <p className="mb-2">Email: {classObj.email}</p>
+              <p className="mb-2">Available seats: {classObj.seats}</p>
+              <p className="mb-2">Price: ${classObj.price}</p>
+
+              <p className="mb-2 flex items-center">
+                Status:
+                {classObj.status === "denied" && (
+                  <>
+                    <FaCircle className="text-red-500 ml-2 mr-1" />
+                    Denied
+                  </>
+                )}
+                {classObj.status === "approved" && (
+                  <>
+                    <FaCircle className="text-green-500 ml-2 mr-1" />
+                    Approved
+                  </>
+                )}
+                {classObj.status === "pending" && (
+                  <>
+                    <MdPendingActions className="text-yellow-500 ml-2 mr-1 text-xl" />
+                    Pending
+                  </>
+                )}
+              </p>
+              <div className="flex">
+                {/* approve and deny buttons */}
                 <button
-                  onClick={() => approveClass(classObj._id)}
-                  disabled={classObj.status !== "pending"}
-                  className="btn btn-sm btn-outline mr-2">
+                  title="Approve"
+                  className="btn btn-sm btn-outline mr-2"
+                  disabled={classObj.status === "approved"}
+                  onClick={() => handleApprove(classObj._id)}>
                   <FaCheck className="text-green-500" />
                 </button>
                 <button
-                  disabled={classObj.status !== "pending"}
-                  onClick={() => denyClass(classObj._id)}
-                  className="btn btn-sm  btn-outline mr-2">
+                  title="Deny"
+                  className="btn btn-sm btn-outline mr-2"
+                  disabled={classObj.status === "denied"}
+                  onClick={() => handleDeny(classObj._id)}>
                   <FaTimes className="text-red-500" />
                 </button>
-              </>
-            )}
-            <button
-              onClick={() => openFeedbackModal(classObj)}
-              className="btn btn-sm btn-primary">
-              <FaComments />
-            </button>
-          </div>
+                {/* feedback modal popup trigger button */}
+                <button
+                  title="Feedback"
+                  className="btn btn-sm btn-primary"
+                  onClick={() => openFeedbackModal(classObj)}>
+                  <FaComments />
+                </button>
+              </div>
+            </div>
+          ))}
         </div>
-      ))}
+      </div>
       {/* Modal */}
       {feedbackModalOpen && (
         <div className="fixed inset-0 flex items-center justify-center z-50">
           <div className="bg-white p-4 shadow-md rounded-lg">
             <h2 className="text-lg font-bold mb-2">Send Feedback</h2>
-            <p className="mb-2">
-              Class: {selectedClass ? selectedClass.name : ""}
-            </p>
+            <p className="mb-2">Class: {selectedClass?.name}</p>
             <textarea
-              value={feedback}
-              onChange={(e) => setFeedback(e.target.value)}
-              className="w-full h-32 border border-gray-300 rounded-md p-2 mb-2"></textarea>
-            <button onClick={sendFeedback} className="btn btn-sm btn-primary">
+              required
+              className="w-full h-32 border border-gray-300 rounded-md p-2 mb-2"
+              value={feedbackText}
+              onChange={(e) => setFeedbackText(e.target.value)}></textarea>
+            <button
+              className="btn btn-sm btn-primary"
+              onClick={() =>
+                handleFeedback(selectedClass?._id, feedbackText) &&
+                closeFeedbackModal()
+              }>
               Send
             </button>
             <button
-              onClick={() => setFeedbackModalOpen(false)}
-              className="btn btn-sm btn-outline ml-2">
+              className="btn btn-sm btn-outline ml-2"
+              onClick={closeFeedbackModal}>
               Cancel
             </button>
           </div>
         </div>
       )}
-    </div>
+    </>
   );
 };
 
