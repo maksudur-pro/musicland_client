02de4590d3bb102f64fbc5edbf8bd5864c40cae8
unknown <ajoy9532@gmail.com>
2023-06-14 00:26:17 +0600

    added section title

--- src/pages/Home/Home/Gallery.jsx
+++ src/pages/Home/Home/Gallery.jsx
@@ -1,55 +1,63 @@
 const Gallery = () => {
   return (
-    <div class="container mx-auto px-5 py-2 lg:px-32 lg:pt-24">
-      <div class="-m-1 flex flex-wrap md:-m-2">
-        <div class="flex w-1/2 flex-wrap">
-          <div class="w-1/2 p-1 md:p-2">
-            <img
-              alt="gallery"
-              class="block h-full w-full rounded-lg object-cover object-center"
-              src="https://images.unsplash.com/photo-1470225620780-dba8ba36b745?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=870&q=80"
-            />
+    <>
+      <div className="mx-auto text-center md:w-4/12 my-2">
+        <h3 className="text-3xl uppercase border-b-4 font-bold py-4">
+          instrument gallery
+        </h3>
+      </div>
+      <div class=" px-5 py-2 lg:px-32 lg:pt-8">
+        {/* instrument gallery  todo */}
+        <div class="-m-1 flex flex-wrap md:-m-2">
+          <div class="flex w-1/2 flex-wrap">
+            <div class="w-1/2 p-1 md:p-2">
+              <img
+                alt="gallery"
+                class="block h-full w-full rounded-lg object-cover object-center"
+                src="https://images.unsplash.com/photo-1470225620780-dba8ba36b745?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=870&q=80"
+              />
+            </div>
+            <div class="w-1/2 p-1 md:p-2">
+              <img
+                alt="gallery"
+                class="block h-full w-full rounded-lg object-cover object-center"
+                src="https://images.unsplash.com/photo-1514119412350-e174d90d280e?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=870&q=80"
+              />
+            </div>
+            <div class="w-full p-1 md:p-2">
+              <img
+                alt="gallery"
+                class="block h-full w-full rounded-lg object-cover object-center"
+                src="https://images.unsplash.com/photo-1415201364774-f6f0bb35f28f?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=870&q=80"
+              />
+            </div>
           </div>
-          <div class="w-1/2 p-1 md:p-2">
-            <img
-              alt="gallery"
-              class="block h-full w-full rounded-lg object-cover object-center"
-              src="https://images.unsplash.com/photo-1514119412350-e174d90d280e?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=870&q=80"
-            />
-          </div>
-          <div class="w-full p-1 md:p-2">
-            <img
-              alt="gallery"
-              class="block h-full w-full rounded-lg object-cover object-center"
-              src="https://images.unsplash.com/photo-1415201364774-f6f0bb35f28f?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=870&q=80"
-            />
-          </div>
-        </div>
-        <div class="flex w-1/2 flex-wrap">
-          <div class="w-full p-1 md:p-2">
-            <img
-              alt="gallery"
-              class="block h-full w-full rounded-lg object-cover object-center"
-              src="https://images.unsplash.com/photo-1568153354382-6bcd1d46568b?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1032&q=80"
-            />
-          </div>
-          <div class="w-1/2 p-1 md:p-2">
-            <img
-              alt="gallery"
-              class="block h-full w-full rounded-lg object-cover object-center"
-              src="https://images.unsplash.com/photo-1563485572084-eb7405aa3b5a?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=870&q=80"
-            />
-          </div>
-          <div class="w-1/2 p-1 md:p-2">
-            <img
-              alt="gallery"
-              class="block h-full w-full rounded-lg object-cover object-center"
-              src="https://images.unsplash.com/photo-1462965326201-d02e4f455804?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=870&q=80"
-            />
+          <div class="flex w-1/2 flex-wrap">
+            <div class="w-full p-1 md:p-2">
+              <img
+                alt="gallery"
+                class="block h-full w-full rounded-lg object-cover object-center"
+                src="https://images.unsplash.com/photo-1568153354382-6bcd1d46568b?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1032&q=80"
+              />
+            </div>
+            <div class="w-1/2 p-1 md:p-2">
+              <img
+                alt="gallery"
+                class="block h-full w-full rounded-lg object-cover object-center"
+                src="https://images.unsplash.com/photo-1563485572084-eb7405aa3b5a?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=870&q=80"
+              />
+            </div>
+            <div class="w-1/2 p-1 md:p-2">
+              <img
+                alt="gallery"
+                class="block h-full w-full rounded-lg object-cover object-center"
+                src="https://images.unsplash.com/photo-1462965326201-d02e4f455804?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=870&q=80"
+              />
+            </div>
           </div>
         </div>
       </div>
-    </div>
+    </>
   );
 };
 
